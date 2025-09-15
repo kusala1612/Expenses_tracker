ed18d2715138aa00d8e3a0f0c1845796121a1cf1
kusala <palurikusalaprabhaprasadini16@gmail.com>
2025-09-15 23:43:58 +0530

    Add init_db.py to create users table for backend setup

--- init_db.py
+++ init_db.py
@@ -7,7 +7,6 @@ import psycopg2
             password="SWoddph65axD4fgQrLUpGtY0nYA35K7F",
             port=5432
         )
-
 cursor = conn.cursor()
 
 cursor.execute("""
@@ -17,8 +16,18 @@ CREATE TABLE IF NOT EXISTS users (
   password TEXT NOT NULL
 );
 """)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS expenses (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  date VARCHAR(20),
+  description TEXT,
+  amount NUMERIC,
+  category VARCHAR(50)
+);
+""")
 
 conn.commit()
 cursor.close()
 conn.close()
-print("✅ Users table created.")
+print("Users table created.")
